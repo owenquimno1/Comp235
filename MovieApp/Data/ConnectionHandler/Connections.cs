d73c5260c5ff1b96605312b43ef6fbcc3bd9cf14
owenquimno1 <quimno6438@saskpolytech.ca>
2024-02-12 16:15:32 -0600

    initalize

---
+++ MovieApp/Data/ConnectionHandler/Connections.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data.ConnectionHandler
+{
+    public class Connections
+    {
+        private static readonly string connectionString;
+        static Connections()
+        {
+            connectionString = < Your connection string goes here >
+        }
+        public static string ConnectionString()
+        {
+            return connectionString;
+        }
+
+
+    }
+}
---
+++ MovieApp/Data/MovieData.cs
@@ -0,0 +1,38 @@
+﻿using MovieApp.Data.ConnectionHandler;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data
+{
+    class MovieData
+    {
+        public List<Movie> MovieSelectAll()
+        {
+            List<Movie> movies = new List<Movie>();
+            SqlConnection con = new
+            SqlConnection(Connections.ConnectionString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT ID, Title, Director FROM Movies";
+using (con)
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    movies.Add(new Movie(
+                    (int)reader["Id"],
+                    (string)reader["Title"],
+                    (string)reader["Director"]));
+                }
+            }
+            return movies;
+        }
+    }
+
+}
+}
